21e301ba65f14c3bea7499ef1fd55933318d4ce6
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-20 21:02:08 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+attr_accessor :first_name, :last_name
+
+def initialize
+  @knowledge = []
+end
+
+def learn(knowledge)
+  @knowledge << knowledge
+end
+
+def knowledge
+  return @knowledge
+end
 
 end
