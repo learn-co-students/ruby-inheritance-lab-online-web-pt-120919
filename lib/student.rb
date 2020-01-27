008a6619e80f93bf40e8942f232b5f1f67538412
Austin Johnson <technicallyaustin@gmail.com>
2020-01-27 19:56:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(string_of_know)
+    @knowledge << string_of_know
+  end
+  
+  def knowledge 
+    @knowledge
+  end
+
 
 end
