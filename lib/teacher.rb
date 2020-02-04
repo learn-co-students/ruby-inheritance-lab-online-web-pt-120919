f78528a2cf05187d12a67a3f14d326c2022af830
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-04 22:32:28 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,10 @@
-class Teacher
+class Teacher < User    # Teacher inherits from the User class
+                        # #first_name has a first name
+                        # #last_name has a last name
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach             # #teach returns a random string of knowledge
+    KNOWLEDGE.sample
+  end
 end
